05b2f9ca4128b2a52f3037b4985b9b31247a6984
Maxime Haineault <max@motion-m.ca>
2012-07-16 16:29:07 -0400

    Now able to extract year, region, vds, and wmi among others. Also added tests

--- libvin/decoding.py
+++ libvin/decoding.py
@@ -2,6 +2,8 @@
 Copyright Maxime Haineault
 VIN Vehicle information number checker,
 
+Reference: http://en.wikipedia.org/wiki/Vehicle_Identification_Number
+
 There are at least four competing standards used to calculate VIN.
 
  * FMVSS 115, Part 565: Used in United States and Canada
@@ -11,7 +13,7 @@ There are at least four competing standards used to calculate VIN.
 
 
 ISO 3779
---------
+========
  
     1 2 3 | 4 5 6 7 8 9 | 10 11 12 13 14 15 16 17
     -----   -----------   -----------------------
@@ -24,7 +26,7 @@ ISO 3779
 
 
 European Union & North America (>500 vehicles/year)
----------------------------------------------------
+===================================================
  
     1 2 3 | 4 5 6 7 8 | 9 | 10 | 11 | 12 13 14 15 16 17
     -----   ---------   -   --   --   -----------------
@@ -43,7 +45,7 @@ European Union & North America (>500 vehicles/year)
 
 
 European Union & North America (<500 vehicles/year)
----------------------------------------------------
+===================================================
  
     1 2 3 | 4 5 6 7 8 | 9 | 10 | 11 | 12 13 14 | 15 16 17
     -----   ---------   -   --   --   --------   --------
@@ -72,12 +74,86 @@ WORLD_MANUFACTURER_REGIONS = {
     'oceania': '67',
 }
 
+YEARS_CODES_PRE_2010 = {
+    'A': 1980, 'L': 1990, 'Y': 2000,
+    'B': 1981, 'M': 1991, '1': 2001,
+    'C': 1982, 'N': 1992, '2': 2002,
+    'D': 1983, 'P': 1993, '3': 2003,
+    'E': 1984, 'R': 1994, '4': 2004,
+    'F': 1985, 'S': 1995, '5': 2005,
+    'G': 1986, 'T': 1996, '6': 2006,
+    'H': 1987, 'V': 1997, '7': 2007,
+    'J': 1988, 'W': 1998, '8': 2008,
+    'K': 1989, 'X': 1999, '9': 2009,
+}
+
+YEARS_CODES_PRE_2040 = {
+    'A': 2010,	'L': 2020, 'Y': 2030,
+    'B': 2011,	'M': 2021, '1': 2031,
+    'C': 2012,	'N': 2022, '2': 2032,
+    'D': 2013,	'P': 2023, '3': 2033,
+    'E': 2014,	'R': 2024, '4': 2034,
+    'F': 2015,	'S': 2025, '5': 2035,
+    'G': 2016,	'T': 2026, '6': 2036,
+    'H': 2017,	'V': 2027, '7': 2037,
+    'J': 2018,	'W': 2028, '8': 2038,
+    'K': 2019,	'X': 2029, '9': 2039,
+}
+
 class Vin(object):
     def __init__(self, vin):
         self.vin = vin
 
     @property
+    def year(self):
+        """
+        Returns the model year of the vehicle
+        """
+        if self.is_pre_2010:
+            return YEARS_CODES_PRE_2010[self.vin[9]]
+        else:
+            return YEARS_CODES_PRE_2040[self.vin[9]]
+
+    @property
+    def is_pre_2010(self):
+        """
+        Returns true if the model year is in the 1980-2009 range
+
+        In order to identify exact year in passenger cars and multipurpose 
+        passenger vehicles with a GVWR of 10,000 or less, one must read 
+        position 7 as well as position 10. For passenger cars, and for 
+        multipurpose passenger vehicles and trucks with a gross vehicle 
+        weight rating of 10,000 lb (4,500 kg) or less, if position 7 is 
+        numeric, the model year in position 10 of the VIN refers to a year 
+        in the range 1980-2009. If position 7 is alphabetic, the model year 
+        in position 10 of VIN refers to a year in the range 2010-2039.
+        """
+        return self.vin[7].isdigit()
+
+    @property
+    def wmi(self):
+        """
+        Returns the World Manufacturer Identifier
+        """
+        return self.vin[0:2]
+
+    @property
+    def vds(self):
+        """
+        Returns the Vehicle Descriptor Section
+        """
+        return self.vin[3:9]
+
+    @property
     def region(self):
+        """
+        Returns the World Manufacturer's Region. Possible results:
+          * africa
+          * asia
+          * north_america
+          * south_america
+          * oceania
+        """
         for region in WORLD_MANUFACTURER_REGIONS:
             codes = WORLD_MANUFACTURER_REGIONS[region]
             if self.vin[0] in codes:
--- tests/test_decoding.py
+++ tests/test_decoding.py
@@ -1,9 +1,70 @@
 # -*- coding: utf-8 -*-
 from nose.tools import assert_equals, raises
 
+from libvin.decoding import Vin
+
+"""
+Test data
+
+JM1BL1SF3A1267720   2010 Mazda 3
+2A4GM684X6R632476   2006 Chrysler Pacifica
+2B3KA43G27H825762   2007 Dodge Charger
+1FAHP3FN8AW139719   2010 Ford Focus
+1GKEV13728J123735   2008 GMC Acadia
+
+"""
+
+
+TEST_DATA = [
+    # http://www.vindecoder.net/?vin=JM1BL1SF3A1267720&submit=Decode
+    {'VIN': 'JM1BL1SF3A1267720',
+     'WMI': 'JM1',
+     'VDS': 'BL1SF3',
+     'VIS': 'A1267720',
+     'MODEL': 'MAZDA3',
+     'MAKE':  'Mazda',
+     'YEAR': 2010,
+     'COUNTRY': 'Japan',
+     'REGION': 'asia',
+     'SEQUANTIAL_NUMBER': 267720,
+     'FEWER_THAN_500_PER_YEAR': False},
+    # http://www.vindecoder.net/?vin=2A4GM684X6R632476&submit=Decode
+    {'VIN': '2A4GM684X6R632476',
+     'WMI': '2A4',
+     'VDS': 'GM684X',
+     'VIS': '6R632476',
+     'MODEL': 'Pacifica',
+     'MAKE':  'Chrysler',
+     'YEAR': 2006,
+     'COUNTRY': 'Canada',
+     'REGION': 'north_america',
+     'SEQUANTIAL_NUMBER': 632476,
+     'FEWER_THAN_500_PER_YEAR': False}
+]
+
 
 class TestDecode(object):
 
-    def test_valid_int(self):
-        assert_equals(1, 1)
+    def test_less_than_500_per_year(self):
+        for test in TEST_DATA:
+            v = Vin(test['VIN'])
+            print "Testing: %s" % test['VIN']
+            assert_equals(v.less_than_500_built_per_year, test['FEWER_THAN_500_PER_YEAR'])
 
+    def test_region(self):
+        for test in TEST_DATA:
+            v = Vin(test['VIN'])
+            print "Testing: %s" % test['VIN']
+            assert_equals(v.region, test['REGION'])
+
+    def test_vds(self):
+        for test in TEST_DATA:
+            v = Vin(test['VIN'])
+            print "Testing: %s" % test['VIN']
+            assert_equals(v.vds, test['VDS'])
+
+    def test_year(self):
+        for test in TEST_DATA:
+            v = Vin(test['VIN'])
+            print "Testing: %s" % test['VIN']
+            assert_equals(v.year, test['YEAR'])
