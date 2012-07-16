6173b466f22c1bd13acd1dcba4691a6a4b0c1df1
Maxime Haineault <max@motion-m.ca>
2012-07-16 15:23:32 -0400

    Added nose test runner

--- setup.py
+++ setup.py
@@ -1,19 +1,25 @@
 from setuptools import setup, find_packages
 import sys, os
 
-version = '0.0.1'
+VERSION = '0.0.1'
+
+REQUIRES = [
+    'nose',
+]
 
 setup(name='libvin',
-      version=version,
-      description="Vehicle Identification Number Library",
-      classifiers=[],
-      keywords='vin vinlib authentic',
-      author='Maxime Haineault (based on the work of Lukasz Szybalski)',
-      author_email='max@motion-m.ca',
-      url='https://github.com/h3/python-libvin',
-      license='LGPL',
-      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[],
+    version=VERSION,
+    description="Vehicle Identification Number Library",
+    classifiers=[],
+    keywords='vin vinlib authentic',
+    author='Maxime Haineault (based on the work of Lukasz Szybalski)',
+    author_email='max@motion-m.ca',
+    url='https://github.com/h3/python-libvin',
+    license='LGPL',
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    include_package_data=True,
+    zip_safe=False,
+    test_suite="nose.collector",
+    tests_require=REQUIRES,
+    install_requires=REQUIRES,
 )
---
+++ tests/test_decoding.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from nose.tools import assert_equals, raises
+
+
+class TestDecode(object):
+
+    def test_valid_int(self):
+        assert_equals(1, 1)
+
