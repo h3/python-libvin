01207a3bbe3b059e4c5d64806d93f4ccd7d6d76d
Dan Kegel <dank@kegel.com>
2016-07-17 19:43:09 -0700

    WMI 5GR

--- libvin/static.py
+++ libvin/static.py
@@ -465,6 +465,7 @@ WMI_MAP = {
     '5FR': 'Acura',
     '5GA': 'Buick',
     '5GN': 'Hummer',
+    '5GR': 'Hummer',   # general motors
     '5J6': 'Honda',
     '5J8': 'Acura',
     '5L': 'Lincoln',
