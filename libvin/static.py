7ab9762434d715d05c87d581508728896008970a
ronan cimadure <ronan.cimadure@gmail.com>
2017-02-28 20:09:48 -0500

    PEP8

--- libvin/static.py
+++ libvin/static.py
@@ -465,8 +465,8 @@ WMI_MAP = {
     '5N1': 'Nissan USA',
     '5NP': 'Hyundai USA',
     '5T': 'Toyota USA - trucks',
-    '5UM' : 'BMW',
-    '5UX' : 'BMW',
+    '5UM': 'BMW',
+    '5UX': 'BMW',
     '6AB': 'MAN Australia',
     '6F4': 'Nissan Motor Company Australia',
     '6F5': 'Kenworth Australia',
@@ -516,4 +516,4 @@ VIN_TRANSLATION = {
     '9': 9, '0': 0, 'U': 4,
 }
 
-VIN_WEIGHT = [8,7,6,5,4,3,2,10,0,9,8,7,6,5,4,3,2]
+VIN_WEIGHT = [8, 7, 6, 5, 4, 3, 2, 10, 0, 9, 8, 7, 6, 5, 4, 3, 2]
